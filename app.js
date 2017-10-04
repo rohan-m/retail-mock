eaa877d0c412d7b60a953824421ab28d65c1986d
Rohan Mahalank <rohan.m.8@gmail.com>
2017-10-04 18:23:00 +0530

    first sample

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//var logger = require('morgan');
+//app.use(logger('combined'));
+
+
+
+var routes = require('./lib/routes.js');
+
+app.get('/:productId', routes.getProduct);
+app.get('/', routes.getProducts);
+
+//error APIs
+app.get('/*',function (req,res)
+{
+    err={};
+    err.code=400;
+    err.msg="Provide a valid path";
+    res.status(err.code).send(JSON.stringify(err, undefined, 2));
+
+});
+
+app.listen(3000, function ()
+{
+    console.log('App listening on port 3000!');
+
+});
